4067a59278c82462bfc10e0e574b01329df7e056
Uribouz <vatcharitopas@gmail.com>
2024-06-18 23:32:55 +0700

    chapter 66: preparing for backend API.

--- 3_hotel-app/starter/hotel-app/src/app/reservation/reservation.service.ts
+++ 3_hotel-app/starter/hotel-app/src/app/reservation/reservation.service.ts
@@ -8,12 +8,6 @@ export class ReservationService {
   private reservations: Reservation[] = [];
   // CRUD
 
-  constructor() {
-    let savedReservations = localStorage.getItem("reservations");
-    this.reservations = savedReservations ? JSON.parse(savedReservations): [];
-    console.log(savedReservations)
-  }
-
   getReservations(): Reservation[] {
     return this.reservations;
   }
@@ -25,19 +19,16 @@ export class ReservationService {
   addReservation(reservation: Reservation): void {
     reservation.id = Date.now().toString();
     this.reservations.push(reservation);
-    localStorage.setItem("reservations", JSON.stringify(this.reservations))
   }
 
   deleteReservation(id: string): void {
     let index = this.reservations.findIndex(res => res.id === id);
     this.reservations.splice(index, 1);
-    localStorage.setItem("reservations", JSON.stringify(this.reservations))
   }
 
   updateReservation(id: string, updatedReservation: Reservation): void {
     let index = this.reservations.findIndex(res => res.id === id)
     this.reservations[index] = updatedReservation;
-    localStorage.setItem("reservations", JSON.stringify(this.reservations))
   }
 
 }
