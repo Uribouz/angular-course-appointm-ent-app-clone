3d31cb1f7de1180659101dc3dc219c8394110bcb
Uribouz <vatcharitopas@gmail.com>
2024-06-20 20:05:07 +0700

    chapter 70: Finishing all CRUD operations with observable.

--- 3_hotel-app/starter/hotel-app/src/app/reservation-form/reservation-form.component.ts
+++ 3_hotel-app/starter/hotel-app/src/app/reservation-form/reservation-form.component.ts
@@ -29,9 +29,10 @@ export class ReservationFormComponent implements OnInit {
     })
     let id = this.activatedRoute.snapshot.paramMap.get('id')
     if (id) {
-      let resevation = this.reservationService.getReservation(id)
-      if (resevation) 
-        this.reservationForm.patchValue(resevation);
+      this.reservationService.getReservation(id).subscribe(reservation => {
+        if (reservation) 
+          this.reservationForm.patchValue(reservation);
+      })
     }
   }
 
@@ -43,9 +44,13 @@ export class ReservationFormComponent implements OnInit {
       if (id) {
         //Dont do this
         // reservation.id = id;
-        this.reservationService.updateReservation(id, reservation)
+        this.reservationService.updateReservation(id, reservation).subscribe(() => {
+          console.log("Update request processed")
+        })
       } else {
-        this.reservationService.addReservation(reservation)
+        this.reservationService.addReservation(reservation).subscribe(() => {
+          console.log("Create request processed")
+        })
       }
       this.router.navigate(['/list'])
     }
