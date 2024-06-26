1786c8365c4f8c376c1d8b878fde193be06f3c36
Uribouz <vatcharitopas@gmail.com>
2024-06-26 19:24:07 +0700

    chapter 91: adding navbar

--- 4_amazing-animal-paintings/starter/src/app/app.module.ts
+++ 4_amazing-animal-paintings/starter/src/app/app.module.ts
@@ -7,6 +7,10 @@ import { AppComponent } from './app.component';
 import { ProductModule } from './product/product.module';
 import {HttpClientModule} from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+
+
 @NgModule({
   declarations: [
     AppComponent
@@ -16,7 +20,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
     AppRoutingModule,
     HttpClientModule,
     ProductModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
