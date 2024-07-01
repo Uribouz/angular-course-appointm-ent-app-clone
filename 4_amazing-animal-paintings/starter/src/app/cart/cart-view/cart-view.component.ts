9fd62a093a2fd82562118cece54724b15a41c4b9
Uribouz <vatcharitopas@gmail.com>
2024-07-01 21:05:18 +0700

    chapter 97: calculating total price.

--- 4_amazing-animal-paintings/starter/src/app/cart/cart-view/cart-view.component.ts
+++ 4_amazing-animal-paintings/starter/src/app/cart/cart-view/cart-view.component.ts
@@ -8,12 +8,18 @@ import { Product } from 'src/app/models/product';
 })
 export class CartViewComponent implements OnInit{
   cartItems: Product[] = [];
+  totalPrice: number = 0;
 
   constructor(private cartService: CartService){}
 
   ngOnInit(): void {
     this.cartService.getCartItems().subscribe(data => {
       this.cartItems = data;
+      this.totalPrice = this.getTotalPrice();
     })
+  }
+
+  getTotalPrice(): number {
+    return this.cartItems.reduce((acc,each) => acc+each.price, 0);
   }
 }
