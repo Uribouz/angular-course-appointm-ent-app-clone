bc870c509cea94dbbfbc5947f4879b45f1b80e30
Uribouz <vatcharitopas@gmail.com>
2024-07-03 19:58:58 +0700

    chapter 99: adding checkout logic.

--- 4_amazing-animal-paintings/starter/src/app/cart/cart-view/cart-view.component.ts
+++ 4_amazing-animal-paintings/starter/src/app/cart/cart-view/cart-view.component.ts
@@ -26,4 +26,8 @@ export class CartViewComponent implements OnInit{
   clearCart(): void {
     this.cartService.clearCart().subscribe();
   }
+
+  checkout(): void {
+    this.cartService.checkout(this.cartItems).subscribe();
+  }
 }
